ffa3b75bb782efdb167980a3a52b4ae4ec96ca0d
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-01-07 14:08:29 +0300

    changed travis

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ DEB_VERSION=$(echo $BUILD_BUILDNUMBER | sed 's/^.*\-//')
 # Set  version in files
 sed -i "s/%VERSION%/${PYT_VERSION}/" pysysdc/__version__.py stdeb.cfg
 
+# https://github.com/pypa/setuptools/issues/3278
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 ./clean.sh
 python3 ./setup.py --command-packages=stdeb.command sdist_dsc --debian-version=${DEB_VERSION}
